8ed2be88d8b7daad30df5546cd6e777ad3f293e5
daibi <dbfornewsletter@outlook.com>
2023-03-09 06:36:48 +0800

    [feature]: transfer facet init

--- contracts/impl/facets/RMRKNestableFacet.sol
+++ contracts/impl/facets/RMRKNestableFacet.sol
@@ -4,7 +4,7 @@
 
 pragma solidity ^0.8.16;
 
-import "./IRMRKNestable.sol";
+import "../../interfaces/IRMRKNestable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
@@ -26,18 +26,6 @@ import { LibMeta } from "../libs/LibMeta.sol";
 contract RMRKNestableFacet is Modifiers {
     using Address for address;
 
-    // -------------------------- MODIFIERS ----------------------------
-
-    /**
-     * @notice Used to verify that the caller is either the owner of the token or approved to manage it by its owner.
-     * @dev If the caller is not the owner of the token or approved to manage it by its owner, the execution will be
-     *  reverted.
-     * @param tokenId ID of the token to check
-     */
-    function _onlyApprovedOrOwner(uint256 tokenId) private view {
-        if (!_isApprovedOrOwner(LibMeta._msgSender(), tokenId)) revert ERC721NotApprovedOrOwner();
-    }
-
     // ------------------------------- ERC721 ---------------------------------
     // /**
     //  * @inheritdoc IERC165
@@ -53,71 +41,6 @@ contract RMRKNestableFacet is Modifiers {
     function balanceOf(address owner) public view returns (uint256) {
         if (owner == address(0)) revert ERC721AddressZeroIsNotaValidOwner();
         return s._balances[owner];
-    }
-
-    ////////////////////////////////////////
-    //              TRANSFERS
-    ////////////////////////////////////////
-
-    function transferFrom(address from, address to, uint256 tokenId) public onlyApprovedOrOwner(tokenId) {
-        _transfer(from, to, tokenId, "");
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId) public  {
-        safeTransferFrom(from, to, tokenId, "");
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public onlyApprovedOrOwner(tokenId) {
-        _safeTransfer(from, to, tokenId, data);
-    }
-
-    /**
-     * @notice Used to safely transfer the token form `from` to `to`.
-     * @dev The function checks that contract recipients are aware of the ERC721 protocol to prevent tokens from being
-     *  forever locked.
-     * @dev This internal function is equivalent to {safeTransferFrom}, and can be used to e.g. implement alternative
-     *  mechanisms to perform token transfer, such as signature-based.
-     * @dev Requirements:
-     *
-     *  - `from` cannot be the zero address.
-     *  - `to` cannot be the zero address.
-     *  - `tokenId` token must exist and be owned by `from`.
-     *  - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
-     * @dev Emits a {Transfer} event.
-     * @param from Address of the account currently owning the given token
-     * @param to Address to transfer the token to
-     * @param tokenId ID of the token to transfer
-     * @param data Additional data with no specified format, sent in call to `to`
-     */
-    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal  {
-        _transfer(from, to, tokenId, data);
-        if (!_checkOnERC721Received(from, to, tokenId, data)) revert ERC721TransferToNonReceiverImplementer();
-    }
-
-    /**
-     * @notice Used to transfer the token from `from` to `to`.
-     * @dev As opposed to {transferFrom}, this imposes no restrictions on msg.sender.
-     * @dev Requirements:
-     *
-     *  - `to` cannot be the zero address.
-     *  - `tokenId` token must be owned by `from`.
-     * @dev Emits a {Transfer} event.
-     * @param from Address of the account currently owning the given token
-     * @param to Address to transfer the token to
-     * @param tokenId ID of the token to transfer
-     * @param data Additional data with no specified format, sent in call to `to`
-     */
-    function _transfer(address from, address to, uint256 tokenId, bytes memory data) internal  {
-        // only owner of will solve the minting & transfer here
-        address owner = LibOwnership.ownerOf(tokenId);
-        if (owner != from) revert ERC721TransferFromIncorrectOwner();
-        if (to == address(0)) revert ERC721TransferToTheZeroAddress();
-
-        s._balances[from] -= 1;
-        _updateOwnerAndClearApprovals(tokenId, 0, to, false);
-        s._balances[to] += 1;
-
-        emit LibERC721.Transfer(from, to, tokenId);
     }
 
 
@@ -286,19 +209,6 @@ contract RMRKNestableFacet is Modifiers {
     //              UTILS
     ////////////////////////////////////////
 
-    /**
-     * @notice Used to check whether the given account is allowed to manage the given token.
-     * @dev Requirements:
-     *
-     *  - `tokenId` must exist.
-     * @param spender Address that is being checked for approval
-     * @param tokenId ID of the token being checked
-     * @return bool The boolean value indicating whether the `spender` is approved to manage the given token
-     */
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view  returns (bool) {
-        address owner = LibOwnership.ownerOf(tokenId);
-        return (spender == owner || LibOwnership.isApprovedForAll(owner, spender) || LibOwnership.getApproved(tokenId) == spender);
-    }
 
     /**
      * @notice Used to invoke {IERC721Receiver-onERC721Received} on a target address.
